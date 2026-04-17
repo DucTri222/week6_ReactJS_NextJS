aaf6822b9c5b92b1e6b2469d2cec6fa6c4ca03bb
DucTri <temporapytest@gmail.com>
2026-04-18 00:49:44 +0700

    bai2_week6_reactJS_nextJS

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,20 @@ import './App.css'
 import Display from './components/Display'
 import Control from './components/Control'
 import React from 'react'
+import { useRecoilState } from 'recoil'
+import { themeState } from './atoms/themeState'
 function App() {
-
+  const [theme, setTheme] = useRecoilState(themeState)
+  const toggleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light")
+  }
   return (
     <>
       <Display/>
       <Control/>
+
+      <h1>Theme this moment: {theme}</h1>
+      <button onClick={toggleTheme}></button>
     </>
   )
 }
